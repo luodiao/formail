f89758d4d4ce16c73e44f40c47c6a50013d6216b
diao.luo <7535886319Diao>
2019-09-23 09:32:59 +0800

    提交脚本

--- application/admin/controller/NewWechatUser.php
+++ application/admin/controller/NewWechatUser.php
@@ -81,7 +81,7 @@ class NewWechatUser extends Backend
                 }
                 if ($result !== false) {
                     $cmd = "cd ".ROOT_PATH." && ./vbot.sh ".ROOT_PATH." ".$params['user_id']." ".$params['port'];
-                    system($cmd);
+                     system($cmd,$retval);
 
                     $this->success();
                 } else {
@@ -164,13 +164,13 @@ class NewWechatUser extends Backend
         $list->status = 0;
         $list->save();
         $cmd = "cd ".ROOT_PATH." && ./vbot.sh ".ROOT_PATH." ".$list->user_id." ".$list->port;
-        system($cmd);
+         system($cmd,$retval);
         $this->success();
     }
 
     public function runwechat(){
         $cmd = "cd /web/formail/ && ./vbot.sh /web/formail/ 1 7536";
-        system($cmd);
+        system($cmd,$retval);
         echo 11111;exit;
     }
     //开启机器人
