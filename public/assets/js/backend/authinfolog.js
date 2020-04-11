dcf50e43b6f2d49c3ca11ebbb0e374d7aaaee0a0
diao.luo <7535886319Diao>
2020-04-11 20:30:45 +0800

    hud

--- public/assets/js/backend/authinfolog.js
+++ public/assets/js/backend/authinfolog.js
@@ -22,8 +22,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
                         {field: 'fk_id', title: __('A_tite'),searchList: {"1":__('A_tite 1'),"2":__('A_tite 2'),"3":__('A_tite 3'),"4":__('A_tite 4'),"5":__('A_tite 5'),"6":__('A_tite 6')}, formatter: Table.api.formatter.normal},
-
-                        {field: 'type', title: __('Type'), searchList: {"0":__('Type 0'),"1":__('Type 1'),"2":__('Type 2'),"3":__('Type 3'),"4":__('Type 4')}, formatter: Table.api.formatter.normal},
                         {field: 'wx_id', title: __('Wx_id')},
                         {field: 'wechat_name', title: '公众号名称'},
                         {field: 'user_name', title: '用户名称'},
