b265f29c84b2fab52c492cd13d49e3e5626b8661
diao.luo <7535886319Diao>
2020-04-17 16:40:40 +0800

    预警管理

---
+++ application/admin/lang/zh-cn/yyzn_works.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Type'       => '类型',
+    'Type 1'     => '服务过期',
+    'Type 2'     => '用户回归',
+    'Type 3'     => '用户注册',
+    'Auth'       => '应用名称',
+    'Auth_id'    => '应用id',
+    'Wx_id'      => '公众号id',
+    'Wxname'     => '公众号名称',
+    'User_id'    => '用户id',
+    'Createtime' => '创建时间',
+    'Desc'       => '预警原因',
+    'Admin_id'   => '管理人id',
+    'Status'     => '状态',
+    'Status 0'   => '待处理',
+    'Status 1'   => '跟进中',
+    'Status 2'   => '已完成',
+    'Assigntime' => '最后分配时间'
+];
---
+++ application/admin/model/YyznWorks.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+
+class YyznWorks extends Model
+{
+
+    
+
+    //数据库
+    protected $connection = 'database';
+    // 表名
+    protected $name = 'yyzn_works';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = 'int';
+
+    // 定义时间戳字段名
+    protected $createTime = 'createtime';
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+        'type_text',
+        'status_text',
+        'assigntime_text'
+    ];
+    
+
+    
+    public function getTypeList()
+    {
+        return ['1' => __('Type 1'), '2' => __('Type 2'), '3' => __('Type 3')];
+    }
+
+    public function getStatusList()
+    {
+        return ['0' => __('Status 0'), '1' => __('Status 1'), '2' => __('Status 2')];
+    }
+
+
+    public function getTypeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['type']) ? $data['type'] : '');
+        $list = $this->getTypeList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+
+    public function getStatusTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['status']) ? $data['status'] : '');
+        $list = $this->getStatusList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+
+    public function getAssigntimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['assigntime']) ? $data['assigntime'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+    protected function setAssigntimeAttr($value)
+    {
+        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
+    }
+
+
+}
