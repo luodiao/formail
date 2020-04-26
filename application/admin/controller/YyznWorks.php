9aa9acc23c7ac446b4b275c308d59ebf0b2d31c2
diao.luo <7535886319Diao>
2020-04-26 20:05:21 +0800

    预警管理

--- application/admin/controller/YyznWorks.php
+++ application/admin/controller/YyznWorks.php
@@ -79,25 +79,30 @@ class YyznWorks extends Backend
      */
     public function adduser($ids = null)
     {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
         if ($this->request->isPost()) {
             $params = $this->request->post("row/a");
             if ($params) {
                 $params = $this->preExcludeFields($params);
-
-                if ($this->dataLimit && $this->dataLimitFieldAutoFill) {
-                    $params[$this->dataLimitField] = $this->auth->id;
-                }
-                $params['admin_id'] = $this->auth->id;
-                $params['adminname'] = $this->auth->nickname;
+                $result = false;
                 Db::startTrans();
                 try {
                     //是否采用模型验证
                     if ($this->modelValidate) {
                         $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
-                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
-                        $this->model->validateFailException(true)->validate($validate);
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                        $row->validateFailException(true)->validate($validate);
                     }
-                    $result = $this->model->allowField(true)->save($params);
+                    $result = $row->allowField(true)->save($params);
                     Db::commit();
                 } catch (ValidateException $e) {
                     Db::rollback();
@@ -112,16 +117,14 @@ class YyznWorks extends Backend
                 if ($result !== false) {
                     $this->success();
                 } else {
-                    $this->error(__('No rows were inserted'));
+                    $this->error(__('No rows were updated'));
                 }
             }
             $this->error(__('Parameter %s can not be empty', ''));
         }
-        $list = $this->model
-                ->where('id',$ids)->find()->toArray();
         $userList = $this->adminModel->select();
         $this->view->assign('userList',$userList);
-        $this->view->assign('list',$list);
+        $this->view->assign('list',$row);
         return $this->view->fetch();
     }
 
