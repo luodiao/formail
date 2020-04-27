9ccf7e533ade2fab3193afdbd64933baf8d8dcb2
diao.luo <7535886319Diao>
2020-04-27 16:16:46 +0800

    添加客情

--- application/admin/controller/YyznWorks.php
+++ application/admin/controller/YyznWorks.php
@@ -166,6 +166,124 @@ class YyznWorks extends Backend
         return $this->view->fetch();
     }
 
+    /**
+     * 查看
+     */
+    public function indexuser()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags']);
+        if ($this->request->isAjax()) {
+            //如果发送的来源是Selectpage，则转发到Selectpage
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
+            }
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $total = $this->model
+                ->alias('w')
+                ->join('users u','u.id=w.user_id','LEFT')
+                ->where($where)
+                ->where('u.mobile','>',0)
+                ->where('w.admin_id',$this->auth->id)
+                ->order('w.id', $order)
+                ->count();
+            $list = $this->model
+                ->alias('w')
+                ->field('w.id,u.image,u.username,u.mobile,w.type,w.desc,w.auth,w.wxname,w.createtime,w.admin_id,w.work_status,w.assigntime')
+                ->join('users u','u.id=w.user_id','LEFT')
+                ->where($where)
+                ->where('u.mobile','>',0)
+                ->where('w.admin_id',$this->auth->id)
+                ->order('w.id', $order)
+                ->limit($offset, $limit)
+                ->select();
+
+            $list = collection($list)->toArray();
+            
+            $result = array("total" => $total, "rows" => $list);
+
+            return json($result);
+        }
+        return $this->view->fetch();
+    }
+    //添加客情
+    public function adduser($ids = null)
+    {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
+        if ($this->request->isPost()) {
+            $params = $this->request->post("row/a");
+            if ($params) {
+                $params = $this->preExcludeFields($params);
+
+                if ($this->dataLimit && $this->dataLimitFieldAutoFill) {
+                    $params[$this->dataLimitField] = $this->auth->id;
+                }
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
+                        $this->model->validateFailException(true)->validate($validate);
+                    }
+                    $result = $row->allowField(true)->save($params);
+                    $logData = array(
+                        'fk_work_id' => $ids,
+                        'work_status' => $params['work_status'],
+                        'admin_id' => $this->auth->id,
+                        'admin_name' => $this->auth->nickname,
+                        'fk_user_id' => $row->user_id,
+                        'desc' => $params['new_desc'],
+                        'createtime' => time(),
+                        );
+                    $this->logmodel->allowField(true)->save($logData);
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were inserted'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        $this->view->assign('list',$row);
+        $user_row = $this->usermodel->get($row->user_id);
+        $this->view->assign('user_row',$user_row);
+        $info_row = $this->authinfomodel->where('fk_user_id',$row->user_id)->select();
+        foreach ($info_row as $key => $info_value) {
+            if($info_value->fk_id == 4 || $info_value->fk_id == 5){
+                $info_value->wx_name = null;
+            }else{
+              $info_value->wx_name = $this->wxmodel->where('id',$info_value->wx_id)->value('name');  
+            }
+        }
+        $this->view->assign('info_row',$info_row);
+        $this->view->assign('time',time());
+        $log_row = $this->logmodel->where('fk_user_id',$row->user_id)->select();
+        $this->view->assign('log_row',$log_row);
+        return $this->view->fetch();
+    }
+
     public function edit($ids = null)
     {
         $row = $this->model->get($ids);
--- public/assets/js/backend/yyzn_works.js
+++ public/assets/js/backend/yyzn_works.js
@@ -74,6 +74,73 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             // 为表格绑定事件
             Table.api.bindevent(table);
         },
+        indexuser: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'yyzn_works/indexuser' + location.search,
+                    // add_url: 'yyzn_works/add',
+                    // edit_url: 'yyzn_works/edit',
+                    // del_url: 'yyzn_works/del',
+                    // multi_url: 'yyzn_works/multi',
+                    table: 'yyzn_works',
+                }
+            });
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id'),operate:false},
+                         {field: 'image', title: '头像', events: Table.api.events.image, formatter: Table.api.formatter.image,operate:false},
+                        {field: 'username', title: '用户昵称'},
+                        {field: 'mobile', title: '联系电话'},
+                        {field: 'type', title: __('Type'), searchList: {"1":__('Type 1'),"2":__('Type 2'),"3":__('Type 3')}, formatter: Table.api.formatter.normal},
+
+                        {field: 'auth', title: __('Auth')},
+                        {field: 'desc', title: __('Desc'),operate:false},
+                        // {field: 'auth_id', title: __('Auth_id')},
+                        // {field: 'wx_id', title: __('Wx_id')},
+                        {field: 'wxname', title: __('Wxname')},
+                        // {field: 'user_id', title: __('User_id')},
+                        
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'admin_id', title: __('Admin_id'),operate:false},
+                        {field: 'work_status', title: __('Status'), searchList: {"0":__('Status 0'),"1":__('Status 1'),"2":__('Status 2')}, formatter: Table.api.formatter.status},
+                        {field: 'assigntime', title: __('Assigntime'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {
+                            field: 'operate',
+                            title: __('Operate'),
+                            table: table,
+                            buttons: [
+                                {
+                                    name: 'addlog', 
+                                    text: '添加客情', 
+                                    title: '添加客情', 
+                                    icon: 'fa fa-plus', 
+                                    classname: 'btn btn-xs btn-danger btn-dialog', 
+                                    url: 'yyzn_works/adduser',
+                                    extend: 'data-area=\'["800px","500px"]\''
+                                }
+                            ],
+                            events: Table.api.events.operate,
+                            formatter: Table.api.formatter.operate
+                        }
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        adduser: function () {
+            Controller.api.bindevent();
+        },
         add: function () {
             Controller.api.bindevent();
         },
