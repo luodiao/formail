ba66d548a258038a1d0af475944e0d07ab7b039a
diao.luo <7535886319Diao>
2020-04-27 15:58:04 +0800

    添加客情

--- application/admin/controller/YyznWorks.php
+++ application/admin/controller/YyznWorks.php
@@ -118,16 +118,16 @@ class YyznWorks extends Backend
                         $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
                         $this->model->validateFailException(true)->validate($validate);
                     }
-                    $result = $this->model->allowField(true)->save($params);
-                    // $logData = array(
-                    //     'fk_work_id' => $ids,
-                    //     'work_status' => $params['work_status'],
-                    //     'admin_id' => $this->auth->id,
-                    //     'fk_user_id' => $row->id,
-                    //     'desc' => $params['new_desc'],
-                    //     'createtime' => time(),
-                    //     );
-                    // $this->logmodel->allowField(true)->save($logData);
+                    $result = $row->allowField(true)->save($params);
+                    $logData = array(
+                        'fk_work_id' => $ids,
+                        'work_status' => $params['work_status'],
+                        'admin_id' => $this->auth->id,
+                        'fk_user_id' => $row->id,
+                        'desc' => $params['new_desc'],
+                        'createtime' => time(),
+                        );
+                    $this->logmodel->allowField(true)->save($logData);
                     Db::commit();
                 } catch (ValidateException $e) {
                     Db::rollback();
