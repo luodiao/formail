ce6236de36aa78bf94b4676c5b222d86d1842f83
diao.luo <7535886319Diao>
2020-04-27 09:48:28 +0800

    预警管理

--- application/admin/controller/YyznWorks.php
+++ application/admin/controller/YyznWorks.php
@@ -33,6 +33,8 @@ class YyznWorks extends Backend
     {
         parent::_initialize();
         $this->model = new \app\admin\model\YyznWorks;
+        $this->adminModel = new \app\admin\model\Admin;
+        $this->usermodel = new \app\admin\model\YyznUsers;
         $this->view->assign("typeList", $this->model->getTypeList());
         $this->view->assign("statusList", $this->model->getStatusList());
     }
@@ -84,16 +86,50 @@ class YyznWorks extends Backend
 
     public function edit($ids = null)
     {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
         if ($this->request->isPost()) {
-                $result = $this->model->where(['id'=>$ids])->update(['work_status'=>'2']);
+            $params = $this->request->post("row/a");
+            if ($params) {
+                $params = $this->preExcludeFields($params);
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                        $row->validateFailException(true)->validate($validate);
+                    }
+                    
+                    $result = $row->allowField(true)->save($params);
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
                 if ($result !== false) {
                     $this->success();
                 } else {
                     $this->error(__('No rows were updated'));
                 }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
         }
-
-        $row = $this->model->get($ids);
         $userList = $this->adminModel->select();
         $this->view->assign('userList',$userList);
         $this->view->assign('list',$row);
